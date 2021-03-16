f2dd75f02cee585844b12f86f14c159b16ba9369
kris <casoltys@gmail.com>
2021-03-16 01:08:40 +0100

    Add custom url generator

--- catalog/models.py
+++ catalog/models.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.utils.text import slugify
 from django.urls import reverse
 
+from nanoid import generate
+
 
 class Content(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -12,6 +14,7 @@ class Content(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200,
                             blank=True)
+    custom_url = models.CharField(max_length=10)
     active = models.BooleanField(default=True)
     url = models.URLField()
     content = models.ImageField(upload_to='content/%Y/%m/%d')
@@ -26,9 +29,12 @@ class Content(models.Model):
         return self.title
 
     def save(self, *args, **kwargs):
-        if not self.slug:
+        if not self.slug and not self.custom_url:
             self.slug = slugify(self.title)
+            self.custom_url = generate
+            ('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+             8)
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('content_detail', args=[str(self.id)])
+        return reverse('content_detail', args=[str(self.custom_url)])
