07adc79228526eac28f2305afd79bb1747da1fae
kris <casoltys@gmail.com>
2021-03-17 23:17:49 +0100

    yet_again

--- catalog/views.py
+++ catalog/views.py
@@ -6,14 +6,14 @@ from .models import Content
 def home(request):
     content = Content.objects.all()
     return render(request,
-                  'catalog/home.html',
+                  'catalog/tmp/home.html',
                   {'content': content})
 
 
 def content_detail(request, custom_url):
     obj = get_object_or_404(Content, custom_url=custom_url)
     return render(request,
-                  'catalog/content_detail.html',
+                  'catalog/tmp/detail.html',
                   {"content": obj})
 
 
